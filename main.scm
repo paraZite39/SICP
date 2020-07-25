f8b15a1b09acc1f3b519e7b46131e59a720c2bc1
paraZite39 <39121521+paraZite39@users.noreply.github.com>
2020-07-25 23:25:55 +0300

    Final ex - 1.29

--- main.scm
+++ main.scm
@@ -1,14 +1,16 @@
-(define (rec-add y k n)
-  (cond ((equal? k n) (y k))
-        ((equal? k 0) (+ (y k) (rec-add y (k + 1) n)))
-        ((equal? (remainder k 2) 0) (+ (* 4 (y k)) (rec-add y (k + 1) n)))
-        (else (+ (* 2 (y k)) (rec-add y (k + 1) n)))))
-
+(define (even? k) (equal? (modulo k 2) 0))
 (define (cube x) (* x x x))
 
+(define (rec-add y k n)
+  (cond ((equal? k n) (y k))
+        ((equal? k 0) (+ (y k) (rec-add y (+ k 1) n)))
+        ((even? k) (+ (* 4 (y k)) (rec-add y (+ k 1) n)))
+        (else (+ (* 2 (y k)) (rec-add y (+ k 1) n)))))
+
 (define (integral f a b n)
-  (let ((h (/ (- b a) n))
-        (k 0))
+  (let ((h (/ (- b a) n)))
       (define (y x) (f (+ a (* x h))))
-      (* (/ h 3) rec-add(y k n))
+      (* (/ h 3) (rec-add y 0 n))
   ))
+
+(integral cube 0 1 1000)
